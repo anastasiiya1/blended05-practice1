d5939e2cf22c9cbfb1e4d319169c09c6f5d39538
Aleksander <aleksander.0705@gmail.com>
2024-05-12 13:09:05 +0300

    add stat

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
 } from 'components';
 import article from 'data/article.json';
 // console.log(article);
+import stats from 'data/stats.json';
 
 export const App = () => {
   return (
@@ -26,7 +27,8 @@ export const App = () => {
         />
 
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" stats={stats} />
+        <Statistics stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
